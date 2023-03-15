59484fbb1c6d488c91dff0e0a04836a45d082459
prohorov01 <alexprohorov01@gmail.com>
2023-03-15 15:48:40 +0200

    update name and number

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const name = "Alex";
-const surname = "Prohorov";
-console.log("hello world $(name), $(surname)");
+const live = "I from in Odessa";
+const yers = ` My name is ${name} I'm 21 yers old  ${live}`;
+console.log(yers);
+let numberOne = 12345;
+let number = {
+  one: "1",
+  two: "2",
+  three: "3",
+  four: "4",
+  fifty: "5",
+};
+console.log(numberOne);
+console.log(Object.values(number));
